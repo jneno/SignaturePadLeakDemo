e6c4a13195aadd47095fd091f7ea2e126b66f197
jneno <james.neno@teamnorthwoods.com>
2018-01-31 16:28:06 -0500

    Initial commit.

---
+++ SignatureTest/Page2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using Xamarin.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SignatureTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var signaturePad = new SignaturePad();
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page1));
+        }
+    }
+}
